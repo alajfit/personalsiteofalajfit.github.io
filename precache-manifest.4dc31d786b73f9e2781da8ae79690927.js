5ba432454aa8bc5b92f7963283f35d91cbb553ee
Alan Fitzpatrick <alajfit@gmail.com>
2020-08-02 19:17:57 +0100

    Updates

---
+++ precache-manifest.4dc31d786b73f9e2781da8ae79690927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c4b602e508e8830f72a6b2b1f5b3a1a",
+    "url": "/site.personal/index.html"
+  },
+  {
+    "revision": "b1b77164e2124a3a08eb",
+    "url": "/site.personal/static/css/main.e3556de3.chunk.css"
+  },
+  {
+    "revision": "18e79b95fbb4b4412cd7",
+    "url": "/site.personal/static/js/2.f5ac47e4.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/site.personal/static/js/2.f5ac47e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1b77164e2124a3a08eb",
+    "url": "/site.personal/static/js/main.43db5461.chunk.js"
+  },
+  {
+    "revision": "d4d8fdd3a6f59ba42272",
+    "url": "/site.personal/static/js/runtime-main.e41add8f.js"
+  }
+]);
